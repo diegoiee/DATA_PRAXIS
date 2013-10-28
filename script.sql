f21038b74708f80ddd95d884bf9924e564700c6d
diegoiee <diegoiee@hotmail.com>
2013-10-28 13:07:47 -0200

    Update script.sql

--- script.sql
+++ script.sql
@@ -267,11 +267,12 @@ falta completar...
 )
 
 
---*/*/*/*/*/-----------------------*/*/*/*/*/*/
-	       	MIGRACION
---*/*/*/*/*/-----------------------*/*/*/*/*/*/
+--/////////////////////////////////////////////////////////////////////////////////////
+--/////////////////////////////////  MIGRACION ///////////////////////////////////////
+--///////////////////////////////////////////////////////////////////////////////////
 
-DATOS_PERSONA
+
+--DATOS_PERSONA
 -------------
 INSERT INTO datos_persona
 
@@ -285,8 +286,7 @@ FROM profesionales
 
 
 
-
-AFILIADOS (joineo los datos personales contra la tabla maestra)
+--AFILIADOS (joineo los datos personales contra la tabla maestra)
 ---------
 
 INSERT INTO AFILIADOS
@@ -297,8 +297,7 @@ WHERE a.dni = b.dni
 
 
 
-
-PROFESIONALES (joineo los datos personales contra la tabla maestra)
+--PROFESIONALES (joineo los datos personales contra la tabla maestra)
 -------------
 
 INSERT INTO AFILIADOS
@@ -306,3 +305,52 @@ INSERT INTO AFILIADOS
 SELECT a.id_plan_medico, b.id_persona, NULL(matricula), NULL(fecha_baja)
 FROM maestra a, datos_persona b
 WHERE a.dni = b.dni
+
+
+
+--TIPO_ESPECIALIDAD
+-----------------
+INSERT INTO TIPO_ESPECIALIDADES
+
+SELECT distinct id_tipo_especialidad, descripcion_tipo_especialidad
+FROM maestra
+
+
+--ESPECIALIDADES
+--------------
+
+INSERT INTO ESPECIALIDADES
+
+SELECT distinct id_especialidad, id_tipo_especialidad, descripcion_especialidad
+FROM maestra
+
+
+--MEDICAMENTOS
+------------
+
+INSERT INTO MEDICAMENTOS
+
+SELECT distinct varchar_medicamento
+FROM maestra
+where varchar_medicamento is not null
+
+
+--AGENDA
+------
+--falta...
+
+
+--CONSULTA
+--------
+--falta...
+
+
+
+--BONOS_COMPRA
+------------
+--falta...
+
+
+--BONOS_FARMACIA
+--------------
+--falta...
