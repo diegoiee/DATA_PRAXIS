e6ade24c8db86a4d89e873f3fe6b72498bfdd3a1
Diego Javier Meligrana <diego.meligrana@gmail.com>
2013-10-28 01:15:19 -0200

    Update script.sql
    CREATE TABLE TIPOS Y ESPECIALIDADES

--- script.sql
+++ script.sql
@@ -171,16 +171,16 @@ CREATE TABLE [DATA_PRAXIS].[PROFESIONALES_ESPECIALIDAD](
 )
 
 
-CREATE TABLE [DATA_PRAXIS].[ESPECIALIDADES]( --OK
-    [id_especialidad] NUMERIC(18,0) IDENTITY (1,1) PRIMARY KEY,
-    [id_tipo_especialidad] INT FOREIGN KEY REFERENCES DATA_PRAXIS.TIPO_ESPECIALIDAD (ID_TIPO_ESPECIALIDAD),
-    [descripcion_especialidad] [varchar](255) NOT NULL
-)
-
-
 CREATE TABLE [DATA_PRAXIS].[TIPO_ESPECIALIDAD]( --OK
     [id_tipo_especialidad] NUMERIC(18,0) IDENTITY (1,1) PRIMARY KEY,
     [descripcion_tipo_especialidad] [varchar](255) NOT NULL
+)
+
+
+CREATE TABLE [DATA_PRAXIS].[ESPECIALIDADES]( --OK
+    [id_especialidad] NUMERIC(18,0) IDENTITY (1,1) PRIMARY KEY,
+    [id_tipo_especialidad] NUMERIC (18,0) FOREIGN KEY REFERENCES DATA_PRAXIS.TIPO_ESPECIALIDAD (ID_TIPO_ESPECIALIDAD),
+    [descripcion_especialidad] [varchar](255) NOT NULL
 )
 
 
